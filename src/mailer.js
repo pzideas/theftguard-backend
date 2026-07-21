df71eb863f4fcf4d573d9357013e239a48746031
Parvez Khalil <parvezkhalil594@gmail.com>
2026-07-21 14:19:08 +0500

    mailer.js

--- src/mailer.js
+++ src/mailer.js
@@ -1,7 +1,9 @@
 const nodemailer = require('nodemailer');
 
 const transporter = nodemailer.createTransport({
-  service: 'gmail',
+  host: 'smtp.gmail.com',
+  port: 465,
+  secure: true,
   auth: {
     user: process.env.SMTP_EMAIL,
     pass: process.env.SMTP_APP_PASSWORD
